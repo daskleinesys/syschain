b63ecf70f375521740fe4c854e2b8402a9e4a379
Thomas Schagerl <thomas.schagerl@gmx.net>
2021-11-15 15:36:29 +0100

    difficulty check us binary i/o char

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto-js');
 
-const NEW_BLOCK_MIN_TIME = 30; // in seconds
-const NEW_BLOCK_MAX_TIME = 90; // in seconds
+const NEW_BLOCK_MIN_TIME = 5; // in seconds
+const NEW_BLOCK_MAX_TIME = 15; // in seconds
 
 class Block {
   /**
@@ -29,6 +29,10 @@ class Block {
   }
 }
 
+function hex2bin(hex) {
+  return (parseInt(hex, 16).toString(2)).padStart(8, '0');
+}
+
 function createNextBlock(block) {
   const now = Math.floor(Date.now() / 1000);
   let { difficulty } = block;
@@ -45,7 +49,18 @@ function createNextBlock(block) {
     const hash = crypto
       .SHA256(`${block.rootHash}${now}${rootHash}${nonce}`)
       .toString(crypto.enc.Hex);
-    if (hash.substring(0, difficulty).split('').every(character => character === 'a')) {
+    let valid = true;
+    for (let i = 0; i < difficulty; i += 1) {
+      const hexPart = Math.floor(i / 8);
+      const hex = hash.substring(hexPart * 2, (hexPart * 2) + 2);
+      const byte = hex2bin(hex);
+      const bit = i % 8;
+      if (byte[bit] !== '0') {
+        valid = false;
+        break;
+      }
+    }
+    if (valid) {
       break;
     }
     nonce += 1;
@@ -77,7 +92,7 @@ console.log(`NEW_BLOCK_MAX_TIME: ${NEW_BLOCK_MAX_TIME}s`);
 console.log('');
 console.log('');
 
-for (let i = 0; i < 10; i += 1) {
+for (let i = 0; i < 50; i += 1) {
   const nextBlock = createNextBlock(currentBlock);
   blockchain.push(nextBlock);
   const delta = Math.floor(Date.now() / 1000) - currentBlock.createdAt;
